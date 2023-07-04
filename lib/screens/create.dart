50a48786bc8f38bba7d1af053085f942dbcff562
muskanlalwani <muskan.lalwani2020@vitbhopal.ac.in>
2023-07-04 16:42:35 +0530

    coomit

--- lib/screens/create.dart
+++ lib/screens/create.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:note_app/constants/colors.dart';
 import 'package:note_app/models/note.dart';
 import 'package:note_app/constants/titles.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class TitleScreen extends StatefulWidget {
   final Note? note;
@@ -29,61 +30,81 @@ class _TitleScreenState extends State<TitleScreen> {
     return titlesList[random.nextInt(titlesList.length)];
   }
 
+  getRandomColor() {
+    Random random = Random();
+    return backgroundColors[random.nextInt(backgroundColors.length)];
+  }
+
+  Future<void> _handleRefresh() async {
+    setState(() {
+      _titleController.text = getRandomTitle();
+    });
+    return Future.delayed(Duration(seconds: 2));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade800,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    padding: const EdgeInsets.all(0),
-                    icon: Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                          color: Colors.grey.shade800.withOpacity(.8),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: const Icon(
-                        Icons.arrow_back_ios_new,
-                        color: Colors.white,
-                      ),
-                    ))
-              ],
-            ),
-            Expanded(
-                child: ListView(
-              children: [
-                TextField(
-                  controller: _contentController,
-                  style: const TextStyle(color: Colors.white, fontSize: 30),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: getRandomTitle(),
-                    hintStyle: const TextStyle(color: Colors.grey),
-                  ),
-                ),
-                TextField(
-                  controller: _contentController,
-                  style: const TextStyle(color: Colors.white),
-                  decoration: const InputDecoration(
+      body: LiquidPullToRefresh(
+        color: Colors.white,
+        height: 150,
+        showChildOpacityTransition: false,
+        backgroundColor: Colors.grey.shade800,
+        animSpeedFactor: 2,
+        onRefresh: _handleRefresh,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      padding: const EdgeInsets.all(0),
+                      icon: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade800.withOpacity(.8),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const Icon(
+                          Icons.arrow_back_ios_new,
+                          color: Colors.white,
+                        ),
+                      ))
+                ],
+              ),
+              Expanded(
+                  child: ListView(
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    style: const TextStyle(color: Colors.white, fontSize: 30),
+                    decoration: InputDecoration(
                       border: InputBorder.none,
-                      hintText:
-                          'Create the content related to the title hinted',
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                      )),
-                ),
-              ],
-            ))
-          ],
+                      hintText: getRandomTitle(),
+                      hintStyle: const TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                  TextField(
+                    controller: _contentController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText:
+                            'Create the content related to the idea title hinted',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        )),
+                  ),
+                ],
+              ))
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
