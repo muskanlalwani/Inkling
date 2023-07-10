fb22d84443e4daeb3ed1282cff5f1c727ca871fc
muskanlalwani <muskan.lalwani2020@vitbhopal.ac.in>
2023-07-10 23:32:15 +0530

    commit

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,7 +4,6 @@ List<Color> backgroundColors = [
   const Color(0xFFCCE5FF), // light blue
   const Color(0xFFD7F9E9), // pale green
   const Color(0xFFFFF8E1), // pale yellow
-  const Color(0xFFF5E6CC), // beige
   const Color(0xFFFFD6D6), // light pink
   const Color(0xFFE5E5E5), // light grey
   const Color(0xFFE6F9FF), // pale blue
@@ -12,6 +11,7 @@ List<Color> backgroundColors = [
   const Color(0xFFFFF3CD), // pale orange
   const Color(0xFFF0E6FF), //lavender
   const Color(0xFFD1F5FF),
+  const Color(0xFFFF8A80),
   const Color(0xFFFFEBBA),
   const Color(0xFFFFC0CB),
   const Color(0xFFE6F3FF),
